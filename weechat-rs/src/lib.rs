3d0e9a65720dc208be92a60fba85e569d5cae1f6
Damir Jelić <poljar@termina.org.uk>
2020-01-12 12:00:30 +0100

    lib: Allow users to create an Error object.
    This is a quick fix, the error type should be expanded so that we can
    let users signal differing errors.

--- weechat-rs/src/lib.rs
+++ weechat-rs/src/lib.rs
@@ -29,7 +29,7 @@ pub trait WeechatPlugin: Sized {
     fn init(weechat: &Weechat, args: ArgsWeechat) -> WeechatResult<Self>;
 }
 
-pub struct Error(c_int);
+pub struct Error(pub c_int);
 pub type WeechatResult<T> = Result<T, Error>;
 
 /// Status values for Weechat callbacks
